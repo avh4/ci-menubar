5feae2e2d3cf3142252ec4dce4763fc148f741d7
Aaron VonderHaar <av@noredink.com>
2017-06-16 14:01:50 -0700

    Send jenkins projects to the menubar

--- main.js
+++ main.js
@@ -4,17 +4,11 @@ var fileUrl = require("file-url");
 var jenkins = require("jenkins-api");
 const { BrowserWindow, app } = require("electron");
 
-// var mb = menubar({
-//   // dir: __dirname,
-//   index: fileUrl(path.join(__dirname, "index.html")),
-//   icon: path.join(__dirname, "icon.png"),
-// });
-//
-//
+const DEBUG = true;
 
 app.on("ready", function() {
   // showOAuth();
-  fetchProjects();
+  showMenubar();
 });
 
 function showOAuth() {
@@ -59,22 +53,49 @@ function showOAuth() {
   );
 }
 
-function fetchProjects() {
-  var user = "";
-  var token = "";
+function fetchProjects(callback) {
+  var user = "avh4";
+  var token = "843ea8d4c4ac796abb7a53aaf491052d";
   var ci = jenkins.init(
     "https://" + user + ":" + token + "@jenkins.noredink.com"
   );
 
+  console.log("Fetching projects from Jenkins...");
+
   ci.all_jobs(function(err, data) {
-    console.log(err, data);
+    // TODO: error checking
+    console.log("Got projects from Jenkins");
+    callback(data);
   });
 }
 
-// mb.on('ready', function ready () {
-//   console.log('app is ready')
-//   console.log(mb.getOption('index'));
-//
-//   // mb.tray.setImage(path.join(__dirname, "icon.png"));
-//   //
-// })
+function showMenubar() {
+  var mb = menubar({
+    // dir: __dirname,
+    index: fileUrl(path.join(__dirname, "index.html")),
+    icon: path.join(__dirname, "icon.png"),
+    width: DEBUG ? 1000 : null,
+    height: DEBUG ? 800 : null
+  });
+
+  mb.on("ready", function ready() {
+    // TODO: why doesn't this run?
+    console.log("app is ready");
+    console.log(mb.getOption("index"));
+
+    // mb.tray.setImage(path.join(__dirname, "icon.png"));
+    //
+  });
+
+  mb.on("after-create-window", function() {
+    if (DEBUG) {
+      mb.window.openDevTools();
+    }
+  });
+  const { ipcMain } = require("electron");
+  ipcMain.on("jenkins-projects", (event, arg) => {
+    fetchProjects(data => {
+      event.sender.send("jenkins-projects-reply", data);
+    });
+  });
+}
