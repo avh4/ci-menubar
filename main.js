3edd68d4e8953601ce512277f362a6f0b0f8d0cf
Aaron VonderHaar <av@noredink.com>
2017-06-16 10:18:19 -0700

    Log in to NRI Jenkins

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
-var menubar = require('menubar')
-var path = require('path');
-var fileUrl = require('file-url');
+var menubar = require("menubar");
+var path = require("path");
+var fileUrl = require("file-url");
+var jenkins = require("jenkins-api");
+const { BrowserWindow, app } = require("electron");
 
-var mb = menubar({
-  // dir: __dirname,
-  index: fileUrl(path.join(__dirname, "index.html")),
-  icon: path.join(__dirname, "icon.png"),
+// var mb = menubar({
+//   // dir: __dirname,
+//   index: fileUrl(path.join(__dirname, "index.html")),
+//   icon: path.join(__dirname, "icon.png"),
+// });
+//
+//
+
+app.on("ready", function() {
+  showOAuth();
 });
 
-mb.on('ready', function ready () {
-  console.log('app is ready')
-  console.log(mb.getOption('index'));
+function showOAuth() {
+  var authWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: "hiddenInset",
+    show: false,
+    "node-integration": false
+  });
+  var authUrl = "https://jenkins.noredink.com";
+  authWindow.loadURL(authUrl);
+  authWindow.show();
+}
 
-  // mb.tray.setImage(path.join(__dirname, "icon.png"));
-})
-
+//
+// var ci = jenkins.init("https://jenkins.noredink.com");
+//
+// ci.all_jobs(function(err, data) {
+//   console.log(err, data);
+// });
+//
+// mb.on('ready', function ready () {
+//   console.log('app is ready')
+//   console.log(mb.getOption('index'));
+//
+//   // mb.tray.setImage(path.join(__dirname, "icon.png"));
+//   //
+// })
