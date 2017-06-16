003ae1ff5e075afb1ec8bacd7ee5ba159c9b1bd9
Aaron VonderHaar <av@noredink.com>
2017-06-16 14:40:50 -0700

    Set up menubar Elm program

--- Main.elm
+++ Main.elm
@@ -3,5 +3,35 @@ module Main exposing (main)
 import Html
 
 
+type alias Model =
+    { theString : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { theString = "Waiting..."
+    }
+
+
+type Msg
+    = NewTheString String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NewTheString string ->
+            ( { model | theString = string }
+            , Cmd.none
+            )
+
+
+main : Program Never Model Msg
 main =
-    Html.text "Hellmo -- HI!!!!"
+    Html.program
+        { init = ( initialModel, Cmd.none )
+        , update = update
+        , subscriptions = \model -> Sub.none
+        , view = \model -> Html.text model.theString
+        }
