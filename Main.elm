685ade878a8b675bfde8c00a1e78b52b480f55c2
Aaron VonderHaar <av@noredink.com>
2017-06-16 17:20:52 -0700

    Show only building projects

--- Main.elm
+++ Main.elm
@@ -2,6 +2,7 @@ port module Main exposing (main)
 
 import Html exposing (Html)
 import Html.Events
+import Json.Decode as Decode
 
 
 type alias Model =
@@ -12,7 +13,16 @@ type alias Model =
 type alias JenkinsProject =
     { name : String
     , url : String
+    , color : Maybe String
     }
+
+
+projectDecoder : Decode.Decoder JenkinsProject
+projectDecoder =
+    Decode.map3 JenkinsProject
+        (Decode.field "name" Decode.string)
+        (Decode.field "url" Decode.string)
+        (Decode.maybe (Decode.field "color" Decode.string))
 
 
 initialModel : Model
@@ -22,14 +32,29 @@ initialModel =
 
 
 type Msg
-    = NewJenkinsProjects (List JenkinsProject)
+    = NewJenkinsProjects (List Decode.Value)
     | OpenExternal String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NewJenkinsProjects projects ->
+        NewJenkinsProjects jsons ->
+            let
+                projects =
+                    jsons
+                        |> List.map (Decode.decodeValue projectDecoder)
+                        |> List.map okOrCrash
+
+                okOrCrash : Result String a -> a
+                okOrCrash result =
+                    case result of
+                        Ok a ->
+                            a
+
+                        Err message ->
+                            Debug.crash message
+            in
             ( { model | jenkinsProjects = projects }
             , Cmd.none
             )
@@ -42,16 +67,30 @@ update msg model =
 
 viewProject : JenkinsProject -> Html Msg
 viewProject project =
-    Html.li []
+    Html.li [] <|
         [ Html.a [ Html.Events.onClick (OpenExternal project.url) ]
             [ Html.text project.name
             ]
         ]
 
 
+filterBuilds : JenkinsProject -> Bool
+filterBuilds project =
+    if project.color == Just "yellow_anime" then
+        True
+    else
+        False
+
+
 view : Model -> Html Msg
 view model =
-    Html.ul [] (List.map viewProject model.jenkinsProjects)
+    Html.div []
+        [ Html.h3 [] [ Html.text "Building" ]
+        , model.jenkinsProjects
+            |> List.filter filterBuilds
+            |> List.map viewProject
+            |> Html.ul []
+        ]
 
 
 main : Program Never Model Msg
@@ -64,7 +103,7 @@ main =
         }
 
 
-port jenkinsProjects : (List JenkinsProject -> msg) -> Sub msg
+port jenkinsProjects : (List Decode.Value -> msg) -> Sub msg
 
 
 port openExternal : String -> Cmd msg
